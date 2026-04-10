993d406b21ac7cf66369bbdb6a9d089f9fb6eb2c
gxytsyt <145750238+gxytsyt@users.noreply.github.com>
2026-04-10 22:59:36 +0800

    Add files via upload

---
+++ src/llamafactory/model/loader.py
@@ -0,0 +1,291 @@
+# Copyright 2025 the LlamaFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import TYPE_CHECKING, Any, Optional, TypedDict
+
+import torch
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoModelForSeq2SeqLM,
+    AutoModelForTextToWaveform,
+    AutoModelForVision2Seq,
+    AutoProcessor,
+    AutoTokenizer,
+)
+from trl import AutoModelForCausalLMWithValueHead
+
+from ..extras import logging
+from ..extras.misc import count_parameters, skip_check_imports, try_download_model_from_other_hub
+from ..extras.packages import is_transformers_version_greater_than
+from .adapter import init_adapter
+from .model_utils.liger_kernel import apply_liger_kernel
+from .model_utils.misc import register_autoclass
+from .model_utils.mod import convert_pretrained_model_to_mod, load_mod_pretrained_model
+from .model_utils.unsloth import load_unsloth_pretrained_model
+from .model_utils.valuehead import load_valuehead_params
+from .patcher import patch_config, patch_model, patch_processor, patch_tokenizer, patch_valuehead_model
+
+if is_transformers_version_greater_than("4.46.0"):
+    from transformers import AutoModelForImageTextToText
+
+if TYPE_CHECKING:
+    from transformers import PretrainedConfig, PreTrainedModel, PreTrainedTokenizer, ProcessorMixin
+
+    from ..hparams import FinetuningArguments, ModelArguments
+
+
+logger = logging.get_logger(__name__)
+
+
+class TokenizerModule(TypedDict):
+    tokenizer: "PreTrainedTokenizer"
+    processor: Optional["ProcessorMixin"]
+
+
+def _get_init_kwargs(model_args: "ModelArguments") -> dict[str, Any]:
+    r"""Get arguments to load config/tokenizer/model.
+
+    Note: including inplace operation of model_args.
+    """
+    skip_check_imports()
+    model_args.model_name_or_path = try_download_model_from_other_hub(model_args)
+    return {
+        "trust_remote_code": model_args.trust_remote_code,
+        "cache_dir": model_args.cache_dir,
+        "revision": model_args.model_revision,
+        "token": model_args.hf_hub_token,
+    }
+
+
+def load_tokenizer(model_args: "ModelArguments") -> "TokenizerModule":
+    r"""Load pretrained tokenizer and optionally loads processor.
+
+    Note: including inplace operation of model_args.
+    """
+    init_kwargs = _get_init_kwargs(model_args)
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=model_args.use_fast_tokenizer,
+            split_special_tokens=model_args.split_special_tokens,
+            padding_side="right",
+            **init_kwargs,
+        )
+    except ValueError:  # try another one
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=not model_args.use_fast_tokenizer,
+            padding_side="right",
+            **init_kwargs,
+        )
+    except Exception as e:
+        raise OSError("Failed to load tokenizer.") from e
+
+    patch_tokenizer(tokenizer, model_args)
+
+    try:
+        processor = AutoProcessor.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=model_args.use_fast_tokenizer,
+            **init_kwargs,
+        )
+    except ValueError:  # try another one
+        processor = AutoProcessor.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=not model_args.use_fast_tokenizer,
+            **init_kwargs,
+        )
+    except Exception as e:
+        logger.info_rank0(f"Failed to load processor: {e}.")
+        processor = None
+
+    # Avoid load tokenizer, see:
+    # https://github.com/huggingface/transformers/blob/v4.40.0/src/transformers/models/auto/processing_auto.py#L324
+    if processor is not None and "Processor" not in processor.__class__.__name__:
+        logger.debug("The loaded processor is not an instance of Processor. Dropping it.")
+        processor = None
+
+    if processor is not None:
+        patch_processor(processor, tokenizer, model_args)
+
+    return {"tokenizer": tokenizer, "processor": processor}
+
+
+def load_config(model_args: "ModelArguments") -> "PretrainedConfig":
+    r"""Load model config."""
+    init_kwargs = _get_init_kwargs(model_args)
+    return AutoConfig.from_pretrained(model_args.model_name_or_path, **init_kwargs)
+
+
+def load_model(
+    tokenizer: "PreTrainedTokenizer",
+    model_args: "ModelArguments",
+    finetuning_args: "FinetuningArguments",
+    is_trainable: bool = False,
+    add_valuehead: bool = False,
+) -> "PreTrainedModel":
+    r"""Load pretrained model."""
+    init_kwargs = _get_init_kwargs(model_args)
+    config = load_config(model_args)
+    patch_config(config, tokenizer, model_args, init_kwargs, is_trainable)
+    apply_liger_kernel(config, model_args, is_trainable, require_logits=(finetuning_args.stage not in ["pt", "sft"]))
+
+    # print('model_args.use_unsloth', model_args.use_unsloth)  # False
+    # print('model_args.adapter_name_or_path', model_args.adapter_name_or_path)  # None
+    # print('is_trainable', is_trainable)  # True
+
+
+    model = None
+    lazy_load = False
+    if model_args.use_unsloth:
+        if model_args.adapter_name_or_path is not None:
+            lazy_load = True
+        elif is_trainable:
+            model = load_unsloth_pretrained_model(config, model_args, finetuning_args)
+
+    # print('model is None', model is None)  # True
+    # print('lazy_load', lazy_load)  # False
+    # print('model_args.mixture_of_depths', model_args.mixture_of_depths)  # None
+
+    if model is None and not lazy_load:
+        init_kwargs["config"] = config
+        init_kwargs["pretrained_model_name_or_path"] = model_args.model_name_or_path
+
+        if model_args.mixture_of_depths == "load":
+            model = load_mod_pretrained_model(**init_kwargs)
+        else:
+            if type(config) in AutoModelForVision2Seq._model_mapping.keys():  # image-text
+                load_class = AutoModelForVision2Seq
+            elif (
+                is_transformers_version_greater_than("4.46.0")
+                and type(config) in AutoModelForImageTextToText._model_mapping.keys()
+            ):  # image-text
+                load_class = AutoModelForImageTextToText
+            elif type(config) in AutoModelForSeq2SeqLM._model_mapping.keys():  # audio-text
+                load_class = AutoModelForSeq2SeqLM
+            elif type(config) in AutoModelForTextToWaveform._model_mapping.keys():  # audio hack for qwen2_5_omni
+                load_class = AutoModelForTextToWaveform
+            else:
+                load_class = AutoModelForCausalLM
+
+            if model_args.train_from_scratch:
+                model = load_class.from_config(config, trust_remote_code=model_args.trust_remote_code)
+            else:
+                model = load_class.from_pretrained(**init_kwargs)
+                if getattr(model.config, "model_type", None) == "qwen2_5_omni":
+                    model = model.thinker  # use part of Omni model
+
+        if model_args.mixture_of_depths == "convert":
+            model = convert_pretrained_model_to_mod(model, config, model_args)
+
+    # print('load_class', load_class)  # AutoModelForVision2Seq
+    # print('model_args.mixture_of_depths', model_args.mixture_of_depths)  # None
+    # print('lazy_load', lazy_load)  # False
+    # print('add_valuehead', add_valuehead)  # False
+    # print('is_trainable', is_trainable)  # True
+
+    # ordhead_name_all = ['two_level_encoder', 'encoder_my', 'decoder_my']
+    # output_path = "para_decoder_my_params11111.txt"
+    # with open(output_path, "w", encoding="utf-8") as f:
+    #     for n, p in model.named_parameters():
+    #         for para_i in ordhead_name_all:
+    #             if para_i in n:
+    #                 f.write(f"********** Name: {n}\n")
+    #                 with torch.no_grad():
+    #                     torch.set_printoptions(threshold=1000, edgeitems=6, linewidth=200)
+    #                     f.write(f"Tensor:\n{p.cpu()}\n\n")
+
+    if not lazy_load:
+        patch_model(model, tokenizer, model_args, is_trainable, add_valuehead)
+        register_autoclass(config, model, tokenizer)
+
+    model = init_adapter(config, model, model_args, finetuning_args, is_trainable)
+
+    ########################## load自己训练的这部分参数
+    from safetensors import safe_open
+    if model_args.adapter_name_or_path is not None and not is_trainable:
+        # ordhead_file = model_args.adapter_name_or_path
+        ordhead_file = model_args.adapter_name_or_path[0] + '/custom_trainable_params.safetensors'
+        with safe_open(ordhead_file, framework="pt", device="cpu") as f:
+            ordhead_paras = {key: f.get_tensor(key) for key in f.keys()}
+        if ordhead_paras is not None:
+            ordhead_paras_new = {}
+            for key_i in ordhead_paras:
+                ordhead_paras_new[key_i.replace('base_model.model.', '')] = ordhead_paras[key_i]
+            model.load_state_dict(ordhead_paras_new, strict=False)
+            logger.info_rank0(f"Loaded ordhead_paras from checkpoint: {ordhead_file}")
+
+    if add_valuehead:
+        model = AutoModelForCausalLMWithValueHead.from_pretrained(model)
+        patch_valuehead_model(model)
+
+        if model_args.adapter_name_or_path is not None:
+            vhead_path = model_args.adapter_name_or_path[-1]
+        else:
+            vhead_path = model_args.model_name_or_path
+
+        vhead_params = load_valuehead_params(vhead_path, model_args)
+        if vhead_params is not None:
+            model.load_state_dict(vhead_params, strict=False)
+            logger.info_rank0(f"Loaded valuehead from checkpoint: {vhead_path}")
+
+    # ordhead_name_all = ['para_encoder_my', 'para_decoder_my', 'two_level_encoder', 'encoder_my_pos_pol', 'decoder_my_key_linear', 'decoder_my_query_linear', 'decoder_my_tanh_linear']
+    ordhead_name_all = ['encoder_my', 'decoder_my', 'two_level_encoder']
+    for name,param in model.named_parameters():
+        for ordhead_name in ordhead_name_all:
+            if ordhead_name in name:
+                param.requires_grad_(True)
+
+    if not is_trainable:
+        model.requires_grad_(False)
+        for param in model.parameters():
+            if param.data.dtype == torch.float32 and model_args.compute_dtype != torch.float32:
+                param.data = param.data.to(model_args.compute_dtype)
+
+        model.eval()
+    else:
+        model.train()
+
+    trainable_params, all_param = count_parameters(model)
+    if is_trainable:
+        param_stats = (
+            f"trainable params: {trainable_params:,} || "
+            f"all params: {all_param:,} || trainable%: {100 * trainable_params / all_param:.4f}"
+        )
+    else:
+        param_stats = f"all params: {all_param:,}"
+
+    logger.info_rank0(param_stats)
+    model_args.print_param_status = True
+    if model_args.print_param_status and int(os.getenv("LOCAL_RANK", "0")) == 0:
+        for name, param in model.named_parameters():
+            print(f"name: {name}, dtype: {param.dtype}, device: {param.device}, trainable: {param.requires_grad}")
+
+
+    # ordhead_name_all = ['two_level_encoder', 'encoder_my', 'decoder_my']
+    # output_path = "para_decoder_my_params33333.txt"
+    # with open(output_path, "w", encoding="utf-8") as f:
+    #     for n, p in model.named_parameters():
+    #         for para_i in ordhead_name_all:
+    #             if para_i in n:
+    #                 f.write(f"********** Name: {n}\n")
+    #                 with torch.no_grad():
+    #                     torch.set_printoptions(threshold=1000, edgeitems=6, linewidth=200)
+    #                     f.write(f"Tensor:\n{p.cpu()}\n\n")
+    #
+    # raise
+
+    return model
